f82dbcfb72211c79d7ac28d82a835246a5accea8
David <flio034@gmail.com>
2018-07-23 00:48:56 -0500

    test db

--- proyecto1/app.js
+++ proyecto1/app.js
@@ -6,36 +6,36 @@ var session    = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
 //connect to MongoDB
-mongoose.connect('mongodb://localhost/DBProyecto1');
+mongoose.connect('mongodb://localhost:27017/DBProyecto1');
 var db = mongoose.connection;
 
 
-const options = {
-  autoIndex: false, // Don't build indexes
-  reconnectTries: 30, // Retry up to 30 times
-  reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0
-}
+// const options = {
+//   autoIndex: false, // Don't build indexes
+//   reconnectTries: 30, // Retry up to 30 times
+//   reconnectInterval: 500, // Reconnect every 500ms
+//   poolSize: 10, // Maintain up to 10 socket connections
+//   // If not connected, return errors immediately rather than waiting for reconnect
+//   bufferMaxEntries: 0
+// }
 
-const connectWithRetry = () => {
-  console.log('MongoDB connection with retry')
-  mongoose.connect("mongodb://mongo:27017/DBProyecto1", options).then(()=>{
-    console.log('MongoDB is connected')
-  }).catch(err=>{
-    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
-    setTimeout(connectWithRetry, 5000)
-  })
-}
+// const connectWithRetry = () => {
+//   console.log('MongoDB connection with retry')
+//   mongoose.connect("mongodb://mongo:27017/DBProyecto1", options).then(()=>{
+//     console.log('MongoDB is connected')
+//   }).catch(err=>{
+//     console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+//     setTimeout(connectWithRetry, 5000)
+//   })
+// }
 
 //connectWithRetry()
 
 db.on('error', err =>{
   console.log('error de conexion')
-  setTimeout(connectWithRetry,5000)
+  //setTimeout(connectWithRetry,5000)
 });
-db.on('connected',  () => {
+db.once('connected',  () => {
   console.log('conexion establecida')
 });
 
